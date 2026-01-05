fc30bb358d1d692680ce51b0f5126b6310040d83
tap-invalues <pavitrav0027@gmail.com>
2026-01-05 13:39:18 +0530

    Deleted unused files

--- script.js
+++
@@ -1,9 +0,0 @@
-const tabs = document.querySelectorAll(".tab");
-
-tabs.forEach(tab => {
-  tab.addEventListener("click", () => {
-    tabs.forEach(t => t.classList.remove("active"));
-    tab.classList.add("active");
-  });
-});
-
